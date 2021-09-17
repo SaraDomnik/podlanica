1a9fe6c644ad55b4f9593b900726e4bfdc83e87e
greenmonster00 <domnik94@gmail.com>
2021-09-17 10:17:38 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+//MAPS
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 45.52718350323489, lng: 13.567225039235982 },
+    zoom: 18,
+    mapId: "d35677e370f6e0cd",
+  });
+}
+
+//45.52718350323489, 13.567225039235982
+
+//MAIN NAV BACKGROUND CHANGE ON SCROLL
+let nav = document.getElementById("navigation");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 60) {
+    return (nav.style.backgroundColor = "#497382");
+  }
+  return (nav.style.backgroundColor = "transparent");
+});
+
+//HAMBURGER MENU
+let menu = document.querySelector(".ham__list");
+let ham = document.querySelector(".ham__btn");
+let xIcon = document.querySelector(".ham__btn--x-icon");
+let menuIcon = document.querySelector(".ham__btn--menu-icon");
+
+ham.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (menu.classList.contains("ham__showMenu")) {
+    menu.classList.remove("ham__showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("ham__showMenu");
+
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+//Toggle menu up when you click on item inside nav
+
+let menuItem = document.querySelectorAll(".ham__item");
+
+menuItem.forEach(function (menuItem) {
+  menuItem.addEventListener("click", toggleMenu);
+});
+
+// //NAVBAR  HIDE ON SCROLL
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 60) {
+//     return document.getElementById("navigation").classList.add("hide");
+//   }
+//   return document.getElementById("navigation").classList.remove("hide");
+// });
+
+// //HAMBURGER MENU SHOW ON SCROLL
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 200) {
+//     return document.getElementById("ham").classList.remove("hide__ham");
+//   }
+//   return document.getElementById("ham").classList.add("hide__ham");
+// });
+
+//MENU VARIABLES
+// const menuHeading = document.getElementById("menu__heading");
+// const menuFood = document.getElementById("menu__food");
+// const menuBg = document.getElementById("menu");
+// const btn = document.getElementById("btn");
+
+// //MENU OPEN
+
+// menuHeading.addEventListener("click", function openMenu() {
+//   menuFood.style.display = "block";
+//   menuHeading.style.display = "none";
+//   menuBg.style.backgroundImage = "url('/Img/bg-fish-2.png')";
+//   menuBg.style.backgroundSize = "auto";
+// });
+
+// //MENU CLOSE
+
+// btn.addEventListener("click", function closeMenu() {
+//   menuFood.style.display = "none";
+//   menuHeading.style.display = "block";
+//   menuBg.style.backgroundImage =
+//     "url('/Img/podlanica_pattern-white--rotated.svg')";
+//   menuBg.style.backgroundSize = "cover";
+// });
---
+++ weather-app.js
@@ -0,0 +1,53 @@
+let temp = document.getElementById("temp");
+let city = document.getElementById("city");
+let description = document.getElementById("description");
+let maxTemp = document.getElementById("max-temp");
+let minTemp = document.getElementById("min-temp");
+
+window.addEventListener("load", () => {
+  if (navigator.geolocation) {
+    const currentApi = `http://api.openweathermap.org/data/2.5/weather?q=piran&units=metric&appid=730aaf925ed5cbb5cd67a136c55fba02`;
+
+    fetch(currentApi)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+
+        temp.textContent = Math.round(data.main.temp) + "\u00B0";
+        city.textContent = data.name;
+        description.textContent = data.weather[0].main;
+        maxTemp.textContent = Math.round(data.main.temp_max) + "\u00B0";
+        minTemp.textContent = Math.round(data.main.temp_min) + "\u00B0";
+      });
+  }
+});
+
+//TIME
+
+let time = document.getElementById("time");
+
+setInterval(function () {
+  let today = new Date();
+  let hours = addZero(today.getHours());
+  let minutes = addZero(today.getMinutes());
+  let currentTime = `${hours}:${minutes}`;
+  time.textContent = currentTime;
+}, 1000);
+
+function addZero(num) {
+  return num < 10 ? `0${num}` : num;
+}
+
+//DATE
+let date = document.getElementById("date");
+let dateObj = new Date();
+let currentDate = dateObj.getDate();
+let month = dateObj.toLocaleString("default", { month: "long" });
+let weekday = dateObj.toLocaleString("default", { weekday: "long" });
+date.innerText = `${weekday}, ${currentDate} ${month}`;
+
+//SUNRISE & SUNSET
+let sunrise = document.getElementById("sunrise");
+let sunset = document.getElementById("sunset");
